9723a77b2c116822de71d9600c135fa639cdce22
NavaneethaKrishnan-S <naveensaravana8@gmail.com>
2026-04-10 16:38:24 +0530

    Array Easy: 485. Max Consecutive Ones

---
+++ src/main/java/org/learn/leetcode/array/MaxConsecutiveOnes.java
@@ -0,0 +1,34 @@
+package org.learn.leetcode.array;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] nums = {1,1,0,1,1,1};
+        var result = findMaxConsecutiveOnes(nums);
+        System.out.println(result);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        var count = 0;
+
+//        for (var num : nums) {
+//            if (num == 1)
+//                count++;
+//            if (num == 0) {
+//                result.add(count);
+//                count = 0;
+//            }
+//        }
+
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return max;
+    }
+}
